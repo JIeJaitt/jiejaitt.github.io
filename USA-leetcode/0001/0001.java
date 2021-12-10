d4c5b1642f832bb51c89d3617423d25685a655e8
JIeJaitt <77219045+JIeJaitt@users.noreply.github.com>
2021-12-10 21:27:11 +0800

    Update 0001.java

--- USA-leetcode/0001/0001.java
+++ USA-leetcode/0001/0001.java
@@ -1,1 +1,9 @@
-
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++)
+            for(int j=0;j<i;j++)
+                if(nums[j] == target - nums[i])
+                                  return new int[] {j,i};
+        return null;
+    }
+}
